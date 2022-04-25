9ad8cf082e0e9411c5614131a432599958a48020
Дмитрий Миронов <mironov_2001@mail.ru>
2022-04-25 17:53:13 +0300

    Update query for postgreSQL.sql
    Вывод всех постов

--- query for postgreSQL.sql
+++ query for postgreSQL.sql
@@ -8,3 +8,10 @@ UPDATE public.chess_player
 	
 DELETE FROM public.chess_player
 	WHERE full_name='Хомуха Иван Петрович';
+
+
+SELECT *FROM post 
+	LEFT JOIN chess_player cp  ON post.id_chess_player = cp.id
+	LEFT JOIN video_lesson vl ON post.id_video_lesson = vl.id
+	LEFT JOIN historical_chess_game hcg ON post.id_historical_chess_game = hcg.id
+	LEFT JOIN theory th ON post.id_theory = th.id;

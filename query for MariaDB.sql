90aa2c353e4f59de16f51ed0e2304c0f9d95f7dd
Дмитрий Миронов <mironov_2001@mail.ru>
2022-05-05 13:32:40 +0300

    Update query for MariaDB.sql

--- query for MariaDB.sql
+++ query for MariaDB.sql
@@ -8,3 +8,19 @@ UPDATE avatar
     
 DELETE FROM avatar
 	WHERE path = 'planet10';
+	
+	
+SELECT user.surname as "Фамилия", user.name as "Имя" FROM user 
+	LEFT JOIN role r ON user.id_role = r.id
+    WHERE r.title = 'Админ';
+    
+SELECT surname as "Фамилия", name as "Имя", t.title as "Название турнира" , t.start_data as "Дата начала турнира"  FROM user u
+	LEFT JOIN `user-tournament-stage` uts ON uts.id_user = u.id
+    LEFT JOIN tournament_stage ts ON uts.id_tournament_stage = ts.id
+    LEFT JOIN tournament t ON ts.id_tournament = t.id
+    WHERE surname = 'Иванов' and name = 'Иван';
+    
+SELECT title as "Название достижение", description as "Описание", surname as "Фамилия", name as "Имя" FROM achievement a
+	RIGHT JOIN `user-achievement` ua ON ua.id_achievement = a.id
+    LEFT JOIN user u ON ua.id_user = u.id
+    WHERE a.title = 'Ultra kill';

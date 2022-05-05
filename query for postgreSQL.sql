e70f89ba08223527b2c4a2400172b6baecc6bc9a
Дмитрий Миронов <mironov_2001@mail.ru>
2022-05-05 12:28:08 +0300

    Update query for postgreSQL.sql
    Display all theoretical entries with type "Open openings"

--- query for postgreSQL.sql
+++ query for postgreSQL.sql
@@ -11,12 +11,16 @@ DELETE FROM public.chess_player
 
 
 SELECT *FROM post 
-	LEFT JOIN chess_player cp  ON post.id_chess_player = cp.id
+	LEFT JOIN chess_player cp ON post.id_chess_player = cp.id
 	LEFT JOIN video_lesson vl ON post.id_video_lesson = vl.id
 	LEFT JOIN historical_chess_game hcg ON post.id_historical_chess_game = hcg.id
 	LEFT JOIN theory th ON post.id_theory = th.id;
 	
 SELECT title as "название", hcg.description as "описание", "id_game(NoSQL)", cp1.full_name as "Полное имя", cp2.full_name as "Полное имя" FROM historical_chess_game hcg
-	LEFT JOIN chess_player cp1  ON hcg.id_chess_player_1 = cp1.id
-	LEFT JOIN chess_player cp2  ON hcg.id_chess_player_2 = cp2.id
+	LEFT JOIN chess_player cp1 ON hcg.id_chess_player_1 = cp1.id
+	LEFT JOIN chess_player cp2 ON hcg.id_chess_player_2 = cp2.id
 	WHERE cp1.full_name = 'Магнус Карлсен' OR cp2.full_name = 'Магнус Карлсен';
+	
+SELECT theory.title as "Название", theory.description as "Описание", tot.title as "Тип теории"  FROM theory
+	LEFT JOIN type_of_theory tot ON theory.id_type_of_theory = tot.id
+	WHERE tot.title = 'Открытые дебюты';

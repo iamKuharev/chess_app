f2222256e0bd1648cdfc55eaccecb6736527c66c
Дмитрий Миронов <mironov_2001@mail.ru>
2022-05-05 13:32:00 +0300

    Update query for MariaDB.sql
    CRUD query

--- query for MariaDB.sql
+++ query for MariaDB.sql
@@ -1,1 +1,10 @@
-
+INSERT INTO avatar(
+	path)
+    VALUES ('planet11');
+    
+UPDATE avatar
+	SET path = 'planet10'
+    WHERE path = 'planet11';
+    
+DELETE FROM avatar
+	WHERE path = 'planet10';

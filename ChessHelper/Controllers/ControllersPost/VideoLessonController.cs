08847f77c112fdfdd2475e899b74f6aa76d42c53
Armadi <mitya.kukharev@mail.ru>
2022-06-01 01:50:28 +0300

    add VideoLessonController

---
+++ ChessHelper/Controllers/ControllersPost/VideoLessonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChessHelper.Domain.Repositories.RepositoriesPost;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChessHelper.Controllers.ControllersPost
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoLessonController : ControllerBase
+    {
+        private IVideoLessonRepository _videoLessonRepository;
+        public VideoLessonController(IVideoLessonRepository videoLessonRepository)
+        {
+            _videoLessonRepository = videoLessonRepository;
+        }
+
+        [HttpGet]
+        [Route("items")]
+        public IActionResult GetAllVideoLessons()
+        {
+            return new OkObjectResult(_videoLessonRepository.GetAllVideoLessons());
+        }
+    }
+}

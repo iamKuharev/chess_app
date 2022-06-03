c1f7e0ff05619478d977efd28324ab23439c4c25
Armadi <mitya.kukharev@mail.ru>
2022-06-03 03:01:13 +0300

    add DbContext for MariaDb

---
+++ ChessHelper.Infrastructure/Repository/RepositoryUser/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChessHelper.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChessHelper.Infrastructure.Repository.RepositoryUser
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<Achievement> Achievements { get; set; }
+        public UserContext(DbContextOptions<UserContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}

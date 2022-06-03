07b5518a4cd9522b11b6d6bed9eede01969345e9
Armadi <mitya.kukharev@mail.ru>
2022-06-03 03:04:05 +0300

    add crud request for achievement

--- ChessHelper.Domain/Entities/EntitiesUser/Achievement.cs
+++ ChessHelper.Domain/Entities/EntitiesUser/Achievement.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChessHelper.Domain.Entities
 {
+    [Table("achievement")]
     public class Achievement
     {
-        public int Id { get; set; }
+        [Column("id")]
+        public int? Id { get; set; }
+        [Column("title")]
         public string Title { get; set; }
+        [Column("description")]
         public string Description { get; set; }
     }
 }
--- ChessHelper.Domain/Repositories/RepositoriesUser/IAchievementRepository.cs
+++ ChessHelper.Domain/Repositories/RepositoriesUser/IAchievementRepository.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChessHelper.Domain.Entities;
 
 namespace ChessHelper.Domain.Repositories.RepositoriesUser
 {
     public interface IAchievementRepository
     {
+        Achievement GetAchievement(int id);
+
+        IList<Achievement> GetAllAchievements();
+
+        Task<bool> AddAchievementAsync(Achievement achievement);
+
+        Task<bool> UpdateAchievementAsync(Achievement achievement);
+
+        Task<bool> DeleteAchievementAsync(int id);
     }
 }

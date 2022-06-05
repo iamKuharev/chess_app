a1b4c619de10d51a9d1ff1bd30e4194c06dfa13d
FiLLiN <serfil1337@mail.ru>
2022-06-06 00:56:30 +0300

    Correct API

--- ChessHelper.Domain/Repositories/RepositoriesAnalytics/IAnalyticsRepository.cs
+++ ChessHelper.Domain/Repositories/RepositoriesAnalytics/IAnalyticsRepository.cs
@@ -6,8 +6,6 @@ namespace ChessHelper.Domain.Repositories.RepositoriesAnalytics
 {
     public interface IAnalyticsRepository
     {
-        public int count_users_in_app();
-
         public int count_games_user_common(int id);
         public int count_games_user_win(int id);
         public int count_games_user_loss(int id);
